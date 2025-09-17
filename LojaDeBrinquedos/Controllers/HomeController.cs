86113765fc7fc9d8ec50a8ec267bc11663ffc5b2
Thiago <thiagodgomesd892@gmail.com>
2025-09-16 21:03:56 -0300

    Alterando a HomeController, configurado o ProdutoRepositorio e fazendo a conexão com o banco de dados no appsettings.json

--- LojaDeBrinquedos/Controllers/HomeController.cs
+++ LojaDeBrinquedos/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using LojaDeBrinquedos.Models;
 using Microsoft.AspNetCore.Mvc;
+using LojaDeBrinquedos.Repositorio;
 
 namespace LojaDeBrinquedos.Controllers
 {
@@ -8,14 +9,17 @@ namespace LojaDeBrinquedos.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private readonly ProdutoRepositorio _produtoRepositorio;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var produtos = await _produtoRepositorio.TodosProdutos();
+            return View(produtos);
         }
 
         
